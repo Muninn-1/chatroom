5b81c76892ff4e6787bd61f282a92ae644401b1f
Oscar GONÇALVES RODRIGUES <o.rodrigues@gop.link>
2019-02-18 18:08:05 +0100

    tuto 3

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
+// Imports
 const path = require('path');
+const http = require('http');
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3002;
+const socketIO = require('socket.io');
 
+// Initialisation 
 const publicPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3002;
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', socket => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected')
+    })
+});
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 })
